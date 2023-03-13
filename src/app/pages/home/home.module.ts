bcb037c82a7bc4fbf3b146d8d32ffed25fc645d0
hamedsoum <hamedsoum4023@gmail.com>
2023-03-13 17:20:06 +0000

    page navigation implementing

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -7,14 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { PickupCallCardComponent } from 'src/app/components/pickup-call-card/pickup-call-card.component';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
-  ],
+    HomePageRoutingModule,
+    SharedModule
+    ],
   declarations: [HomePage]
 })
 export class HomePageModule {}
